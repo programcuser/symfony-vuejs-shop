1ec2ed4e49bf4083b115b592f402fe61630e9535
ProgramC <krygrish@yandex.ru>
2022-09-30 18:05:11 +0300

    Add smtp server in mailtrap. Add flash messages for registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -84,12 +84,12 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('homepage'); //app_register
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('homepage'); //app_register
     }
 }
