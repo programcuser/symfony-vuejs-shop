c5111ba33af71587db8b0cd0ddbf7be7f05ee43f
ProgramC <krygrish@yandex.ru>
2022-10-03 17:46:54 +0300

    Add admin role check for authorization admin form

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -26,8 +26,9 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'admin_security_logout')]
-    public function logout(): void
+    public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->redirectToRoute('admin_security_login');
+        // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
--- src/Security/Admin/LoginFormAuthenticator.php
+++ src/Security/Admin/LoginFormAuthenticator.php
@@ -69,7 +69,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
 
-        if (!$user) {
+        if (!$user || !in_array('ROLE_ADMIN', $user->getRoles())) {
             throw new UsernameNotFoundException('Email could not be found.');
         }
 
